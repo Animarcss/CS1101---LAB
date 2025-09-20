d42e4b4bb717db8d865f4c47733769e301abc3da
Animarcs <bilayeparth@gmail.com>
2025-09-20 19:49:58 +0530

    misc

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,7 +1,2 @@
 #include <stdio.h>
-void main() { 
-    int f=1;
-    for (int j=1;j<5;j++) {
-    printf("%d ",f);
-    f*=2*j*(2*j-1);    // f in each iteration is (2j)!
-    } }
+int main() { printf("Hello world!"); return 0; }
