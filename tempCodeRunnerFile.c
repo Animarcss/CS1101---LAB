3ae3384b73efeb807499a6252f17dfac16ff666f
Animarcs <bilayeparth@gmail.com>
2025-10-30 00:25:47 +0530

    CS

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    int a=4294967295;
-    printf("%u",a);
+    int arr[5]={1,2,3,4,5};
+    int *p=arr;
+    printf("%d\n",*p);
+    int **p1=&p;
+    printf("%p\n",p);
+    printf("%p\n",arr);
+    printf("%p\n",&arr[0]);
+    printf("%p\n",p1);
+    printf("%p\n",&p);
+    printf("%p\n",&p1);
+    printf("%p\n",&arr);
 }
